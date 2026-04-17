0d116be2d25792376b206c5a290931479840c533
Sirley Krynski <sirleykrynski1@gmail.com>
2026-04-17 09:39:49 -0300

    Carrusel

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -7,7 +7,7 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400&family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}"> ?v={{ time() }}
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}"> 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
