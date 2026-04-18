499f6e5ff75b15db5fbdd7790dc4606618f39444
Sirley Krynski <sirleykrynski1@gmail.com>
2026-04-18 16:20:13 -0300

    carrusel

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Ruta para la página principal
 Route::get('/', function () {
     return view('inicio');
 });
 
-// Ruta para Quiénes Somos (crearás la vista despues)
 Route::get('/quienes-somos', function () {
     return view('quienes-somos');
 });
 
-// Ruta para el Catálogo
 Route::get('/catalogo', function () {
     return view('catalogo');
 });
 
-// Ruta para el Contacto (Guía 4)
 Route::get('/contacto', function () {
     return view('contacto');
 });
